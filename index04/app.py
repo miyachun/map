48764ecfcfcc37648c8ce19321013eaee6044ed4
miyachun <miyachun808@gmail.com>
2023-12-25 13:25:57 +0800

    first commit

--- index04/app.py
+++ index04/app.py
@@ -1,47 +1,18 @@
+import requests
 import pandas as pd
 import json,urllib.request
-from flask import Flask, render_template, request
-app = Flask(__name__)
+#25.047846496934394, 121.51740617725997
+#zh-TW
+api_key = 'XXXXX'
+
+category_response = requests.get(f'https://api.tomtom.com/search/2/nearbySearch/.json?lat=25.047846496934394&lon=121.51740617725997&limit=5&radius=10000&language=zh-TW&categorySet=7315&view=Unified&relatedPois=off&key={api_key}')
+category_data = category_response.json()
+
+
+with open('data.json', 'w') as f:
+    json.dump(category_data, f)
 
 
 f = open('data.json')
 dataf = json.load(f)
-
-getAll={"Pname":[],"Paddress":[],"PositionLat":[],"PositionLon":[]}
-
-Pname = {"name":[]}
-Paddress={"address":[]}
-PositionLat={"positionLat":[]}
-PositionLon={"positionLon":[]}
-bb=[]
-RName=[]
-Lon=[]
-Lat=[]
-@app.route('/', methods=('GET', 'POST'))
-def index():
-    if request.method == 'POST':
-        dropdownval = request.form.get('Gcity')
-        bb.append(dropdownval)
-        for key, value in getAll.items():
-            for i in value:
-                if i == dropdownval:
-                    print(value.index(i))
-                    Lon.append(getAll['PositionLon'][value.index(i)])
-                    Lat.append(getAll['PositionLat'][value.index(i)])
-                    
-        return render_template('index05.html', getAll=getAll,bb=bb,RName=RName,Lon=Lon,Lat=Lat)
-    i = 0
-    while i < 5:
-        getAll['Pname'].append(dataf['results'][i]['poi']['name'])
-        getAll['Paddress'].append(dataf['results'][i]['address']['freeformAddress'])
-        getAll['PositionLat'].append(dataf['results'][i]['position']['lat'])
-        getAll['PositionLon'].append(dataf['results'][i]['position']['lon'])
-        RName.append(dataf['results'][i]['poi']['name']) 
-        i += 1
-
-    return render_template('index05.html', getAll=getAll,bb=bb,RName=RName,Lon=Lon,Lat=Lat)   
-
-
-
-if __name__ == '__main__':
-    app.run()
+print(dataf)
